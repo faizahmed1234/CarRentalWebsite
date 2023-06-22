ac73e343440081b34340c98a6924fe8cbce8514c
faizahmed <faizahmed01072@gmail.com>
2023-06-22 11:25:48 +0500

    first push

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+              This website is an excellent resource for anyone looking to rent a car.
+              It offers a wealth of information about the rental process, including tips for getting the best deals,  with articles on a variety of topics related to car rentals.
+              Additionally, the website is transparent about pricing and fees,
+              making it easy to select the rental car that best fits your needs and budget.
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Fuel and Expense Tracking
+
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Service History
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Maintenance and tracking
+
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Vehicle Profile Management
+
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/UI/Testimonial.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "../../styles/testimonial.css";
+
+import ava01 from "../../assets/all-images/ava-1.jpg";
+import ava02 from "../../assets/all-images/ava-2.jpg";
+import ava03 from "../../assets/all-images/ava-3.jpg";
+import ava04 from "../../assets/all-images/ava-4.jpg";
+
+const Testimonial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        The website offered a wide selection of cars at affordable prices.
+         I was able to find the perfect car for my trip without breaking the bank.
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava01} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Alex hills</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        The customer service was excellent. The representative was friendly and helpful,
+        and made the rental process a breeze.
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava02} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I had a great experience renting a car from this website. 
+        The process was quick and easy, and the car was in great condition.</p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava03} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">watson</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        The website was easy to navigate and car was clean and well-maintained
+        I was able to find the information I needed quickly and easily.
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava04} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Emily </h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default Testimonial;
